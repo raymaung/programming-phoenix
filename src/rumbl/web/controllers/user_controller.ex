4154dd28856de310fc4cd17990dfeed558daaafd
Raymond Maung <raymaung@gmail.com>
2016-08-27 10:54:08 +1000

    chapter 05 - make authenticate as plug

--- src/rumbl/web/controllers/user_controller.ex
+++ src/rumbl/web/controllers/user_controller.ex
@@ -3,6 +3,8 @@ defmodule Rumbl.UserController do
 
   alias Rumbl.User
 
+  plug :authenticate when action in [:index, :show]
+
   def new(conn, _params) do
     changeset = User.changeset(%User{})
     render conn, "new.html", changeset: changeset
@@ -22,13 +24,8 @@ defmodule Rumbl.UserController do
   end
 
   def index(conn, _params) do
-    case authenticate(conn) do
-      %Plug.Conn{halted: true} = conn ->
-        conn
-      conn ->
-        users = Repo.all User
-        render conn, "index.html", users: users
-    end
+    users = Repo.all User
+    render conn, "index.html", users: users
   end
 
   def show(conn, %{"id" => id}) do
@@ -36,7 +33,7 @@ defmodule Rumbl.UserController do
     render conn, "show.html", user: user
   end
 
-  defp authenticate(conn) do
+  defp authenticate(conn, _opts) do
     if conn.assigns.current_user do
       conn
     else
