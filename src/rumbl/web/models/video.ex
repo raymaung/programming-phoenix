d155003f1e6d6394e2c8e55406afa686e9d7adf7
Raymond Maung <raymaung@gmail.com>
2016-08-28 19:52:54 +1000

    chapter 09 - add slug to video

--- src/rumbl/web/models/video.ex
+++ src/rumbl/web/models/video.ex
@@ -20,6 +20,21 @@ defmodule Rumbl.Video do
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, @required_fields, @optional_fields)
+    |> slugify_title()
     |> assoc_constraint(:category)
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
   end
 end
