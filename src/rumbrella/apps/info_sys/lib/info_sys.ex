493ef57840ce2c8394c7c0fedcc6da06d3225968
Raymond Maung <raymaung@gmail.com>
2017-01-07 18:28:54 +1100

    chapter 12 - move info_sys.ex from rumbl

--- src/rumbrella/apps/info_sys/lib/info_sys.ex
+++ src/rumbrella/apps/info_sys/lib/info_sys.ex
@@ -1,20 +1,107 @@
 defmodule InfoSys do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
+  @backends [InfoSys.Wolfram]
+
+  defmodule Result do
+    defstruct score: 0, text: nil, url: nil, backend: nil
+  end
+
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
+    InfoSys.Supervisor.start_link()
+  end
 
-    # Define workers and child supervisors to be supervised
-    children = [
-      # Starts a worker by calling: InfoSys.Worker.start_link(arg1, arg2, arg3)
-      # worker(InfoSys.Worker, [arg1, arg2, arg3]),
-    ]
+  def start_link(backend, query, query_ref, owner, limit) do
+    backend.start_link query, query_ref, owner, limit
+  end
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: InfoSys.Supervisor]
-    Supervisor.start_link(children, opts)
+  def compute(query, opts \\ []) do
+    limit = opts[:limit] || 10
+    backends = opts[:backends] || @backends
+
+    backends
+    |> Enum.map(&spawn_query(&1, query, limit))
+    |> await_results(opts)
+    |> Enum.sort(&(&1.score >= &2.score))
+    |> Enum.take(limit)
+  end
+
+  defp spawn_query(backend, query, limit) do
+    query_ref = make_ref()
+    opts = [backend, query, query_ref, self(), limit]
+
+    {:ok, pid} = Supervisor.start_child(InfoSys.Supervisor, opts)
+
+    monitor_ref = Process.monitor(pid)
+    {pid, monitor_ref, query_ref}
+  end
+
+  defp await_results(children, opts) do
+
+    #
+    # timeout is for the total wait, not for each
+    #
+    timeout = opts[:timeout] || 5000
+
+    #
+    # 1. Create a timer to send :timedout at <timeout>
+    # to be received in await_result
+    #
+    timer = Process.send_after(self(), :timedout, timeout)
+
+    results = await_result children, [], :infinity
+    cleanup(timer)
+    results
+  end
+
+  defp await_result([head | tail], acc, timeout) do
+    {pid, monitor_ref, query_ref } = head
+
+    receive do
+      {:results, ^query_ref, results} ->
+        Process.demonitor(monitor_ref, [:flush])
+        await_result(tail, results ++ acc, timeout)
+
+      {:DOWN, ^monitor_ref, :process, ^pid, _reason} ->
+        await_result(tail, acc, timeout)
+
+      #
+      # 2. receive :timedout from timer
+      #
+      :timedout ->
+        kill(pid, monitor_ref)
+
+        #
+        # 3. reset the <timeout> to 0
+        #
+        await_result(tail, acc, 0)
+
+    after
+      timeout ->
+        kill(pid, monitor_ref)
+        await_result(tail, acc, 0)
+    end
+  end
+
+  defp await_result([], acc, _) do
+    acc
+  end
+
+  defp kill(pid, ref) do
+    Process.demonitor(ref, [:flush])
+    Process.exit(pid, :kill)
+  end
+
+  defp cleanup(timer) do
+    #
+    # 4. cancel timer, inf case it wasn't yet triggered
+    # flush the :timedout message if it was already sent
+    #
+    :erlang.cancel_timer(timer)
+    receive do
+      :timedout -> :ok
+    after
+      0 -> :ok
+    end
   end
 end
